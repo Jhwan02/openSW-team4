94841b5b5c81bcc9b5919892caf4faeca50ecbbb
YHFTF <yunghy1@chungbuk.ac.kr>
2024-11-22 16:19:13 +0900

    trick

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -10,5 +10,7 @@ public class MainController {
     public String root() {
 		return "main_page"; // main_page.html을 반환
     }
-}
-
+  @GetMapping("/trick")
+    public String trick() {
+      return "trick";
+  }
