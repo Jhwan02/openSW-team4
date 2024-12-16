a81c406f9812c7eb7d992e34e0a5c793d4325597
YTFTF <yunghy1@chungbuk.ac.kr>
2024-12-16 20:33:46 +0900

    리팩토링: 로그인 버튼 ID 변경 및 마이페이지 버튼 표시 로직 수정

--- src/main/resources/static/login/logincheck.js
+++ src/main/resources/static/login/logincheck.js
@@ -11,27 +11,24 @@ document.addEventListener("DOMContentLoaded", () => {
             if (data.loggedIn) {
                 // 로그인 상태일 때
                 //alert(`로그인 상태입니다. 사용자 이름: ${data.username}, 아이디: ${data.id}`);
-                document.getElementById("loginButton").style.display = "none"; // 로그인 버튼 숨기기
                 document.getElementById("myPageButtonContainer").style.display = "block"; // 마이페이지 버튼 보이기
-                
                 const myPageButton = document.getElementById("myPageButton");
                 myPageButton.onclick = function() {
                     window.location.href = "/mypage"; // 마이페이지로 이동
                 }
-                if (window.location.pathname === '/'){
-                    document.getElementById("mainLoginButton").style.display = "none"; // 메인 로그인 버튼 보이기
-                }
+                document.getElementById("mainLoginButton").style.display = "none"; // 메인 로그인 버튼 숨기기
+                
+
                 
  
         }
             else {
                 // 로그아웃 상태일 때
                 //alert("로그아웃 상태입니다!");
-                document.getElementById("loginButton").style.display = "block"; // 로그인 버튼 표시
-                document.getElementById("myPageButtonContainer").style.display = "none"; // 마이페이지 버튼 보이기
-                if (window.location.pathname === '/'){
-                    document.getElementById("mainLoginButton").style.display = "block"; // 메인 로그인 버튼 보이기
-                }
+                document.getElementById("loginModalButton").style.display = "block"; // 로그인 버튼 표시
+                document.getElementById("myPageButtonContainer").style.display = "none"; // 마이페이지 버튼 숨기기기
+                document.getElementById("mainLoginButton").style.display = "block"; // 메인 로그인 버튼 보이기
+                
                 
             }
         })
